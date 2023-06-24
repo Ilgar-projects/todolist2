c7410ccd714c3d75758535069a4a4688a08950f6
Ilgar-projects <okurok3@gmail.com>
2023-06-24 17:12:28 +0300

    в postgres2 в HOST поставил postgres

--- todolist/settings.py
+++ todolist/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 }
